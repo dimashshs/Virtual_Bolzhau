4d03d71d2fc82d83579fa793c92758f3517d5505
dimashshs <93972557+dimashshs@users.noreply.github.com>
2025-04-07 18:23:11 +0500

    Update config.ts

--- bolzhau.ai-main/app/src/config.ts
+++ bolzhau.ai-main/app/src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://192.168.10.12:2423/api/";
+export const API_BASE_URL = "http://192.168.10.12:2423/";
 export const BIGRAM_ENDPOINT = `${API_BASE_URL}bigram/`;
 export const TRIGRAM_ENDPOINT = `${API_BASE_URL}trigram/`;
