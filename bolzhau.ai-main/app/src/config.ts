da9ed87400b6427159cd41c7ad12fcc6388a491d
dimashsh <kusain03@gmail.com>
2025-04-17 18:01:03 +0500

    change config

--- bolzhau.ai-main/app/src/config.ts
+++ bolzhau.ai-main/app/src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://192.168.185.134/";
+export const API_BASE_URL = "http://192.168.185.134/api/";
 export const BIGRAM_ENDPOINT = `${API_BASE_URL}bigram/`;
 export const TRIGRAM_ENDPOINT = `${API_BASE_URL}trigram/`;
